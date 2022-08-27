a2007ceb3310cbdce6eafd9df96451ab0dd0a66d
zhangcc <zcc136314853@163.com>
2022-08-27 18:28:19 +0800

    禁止输出PG_CONF

--- measure_NL.py
+++ measure_NL.py
@@ -11,4 +11,4 @@ if __name__ == '__main__':
     start_time = '2022-01-01'
     end_time = datetime.datetime.now().strftime('%Y-%m-%d')
     feature_info = pd.read_sql_query("SELECT t.ts_code, t.trade_date, t.field, t.value FROM public.t_feature_numberic t where t.field='NL' and t.trade_date > '2022-01-01' order by t.trade_date", engine_finance_db)
-    send("新高新低指标(NHNL)", feature_info.to_html(), "{}至{}期间每日新低指标".format(start_time, end_time))
+    send("新低指标(NHNL)", feature_info.to_html(), "{}至{}期间每日新低指标".format(start_time, end_time))
