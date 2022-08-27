74103d49ea05ef4fd18f3f32aa0733081de6b210
zhangcc <zcc136314853@163.com>
2022-08-27 18:01:04 +0800

    新增新高指标发布任务、新低指标发布任务

--- measure_NH.py
+++ measure_NH.py
@@ -1,6 +1,6 @@
 """
-cron: 0 30 8 * * ?
-new Env('量化--新高新低指标');
+cron: 5 30 8 * * ?
+new Env('量化--新高指标');
 """
 
 import pandas as pd
@@ -10,5 +10,5 @@ if __name__ == '__main__':
     engine_finance_db = pg_engine_finance()
     start_time = '2022-01-01'
     end_time = datetime.datetime.now().strftime('%Y-%m-%d')
-    feature_info = pd.read_sql_query("SELECT t.ts_code, t.trade_date, t.field, t.value FROM public.t_feature_numberic t where t.field='NHNL' and t.trade_date > '2022-01-01' order by t.trade_date", engine_finance_db)
+    feature_info = pd.read_sql_query("SELECT t.ts_code, t.trade_date, t.field, t.value FROM public.t_feature_numberic t where t.field='NH' and t.trade_date > '2022-01-01' order by t.trade_date", engine_finance_db)
     send("新高新低指标(NHNL)", feature_info.to_html(), "{}至{}期间每日新高新低指标".format(start_time, end_time))
