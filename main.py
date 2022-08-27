4848e43ae4a0b3e324816c6ad92d75d74ca7b7de
zhangcc <zcc136314853@163.com>
2022-08-27 12:28:12 +0800

    添加测试脚本

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+from notify import base_notify
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
+    print_hi('PyCharm')
+    base_notify.send("测试", "测试", "测试")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
