9f77e4c3968580f90732b2bb9cbcbed236c0a4d4
zhangcc <zcc136314853@163.com>
2022-09-05 21:14:45 +0800

    数据显示周期改为30天且按倒序排列

--- measure_NH.py
+++ measure_NH.py
@@ -8,7 +8,9 @@ from notify import pg_engine_finance, send
 import datetime
 if __name__ == '__main__':
     engine_finance_db = pg_engine_finance()
-    start_time = '2022-01-01'
+    # start_time = '2022-01-01'
+    start_time = (datetime.datetime.now() - datetime.timedelta(days=30)).strftime('%Y-%m-%d')
     end_time = datetime.datetime.now().strftime('%Y-%m-%d')
-    feature_info = pd.read_sql_query("SELECT t.ts_code, t.trade_date, t.field, t.value FROM public.t_feature_numberic t where t.field='NH' and t.trade_date > '2022-01-01' order by t.trade_date", engine_finance_db)
+
+    feature_info = pd.read_sql_query("SELECT t.ts_code, t.trade_date, t.field, t.value FROM public.t_feature_numberic t where t.field='NH' and t.trade_date > '2022-01-01' order by t.trade_date desc", engine_finance_db)
     send("新高指标(NH)", feature_info.to_html(), "{}至{}期间每日新高指标".format(start_time, end_time))
