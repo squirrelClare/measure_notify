e329e1ddc5c0c82a063e5385a6354891b324ce5c
zhangcc <zcc136314853@163.com>
2022-10-25 21:45:44 +0800

    更新slope_ema的逻辑

--- measure_SLOPE_EMA.py
+++ measure_SLOPE_EMA.py
@@ -18,23 +18,25 @@ if __name__ == '__main__':
     last_cal_day = cal_day_info['cal_date'].values[-1]
 
     #   抽取特征数据
-    sql_feature_ema65_slope ='''select company, trade_date, field, value 
+    sql_feature_ema65_slope ='''select company, trade_date, field, value, ts_code 
     from (select * from t_feature_numberic where field ='SLOPE_EMA_65' and trade_date ='{0}' and value > 0 limit 200) a left join t_tscode_company b on a.ts_code =b.ts_code order 
     by value desc '''.format(last_cal_day)
     feature_info_ema65_slope = pd.read_sql_query(sql_feature_ema65_slope, engine_finance_db)
 
+    ts_code_set = ','.join(['\'%s\''.format(e) for e in feature_info_ema65_slope['ts_code']])
+
     #   抽取特征数据
     sql_feature_ema13_slope ='''select company, trade_date, field, value 
-    from (select * from t_feature_numberic where field ='SLOPE_EMA_13' and trade_date ='{0}' and value > 0 limit 200) a left join t_tscode_company b on a.ts_code =b.ts_code order 
-    by value desc'''.format(last_cal_day)
+    from (select * from t_feature_numberic where field ='SLOPE_EMA_13' and trade_date ='{0}' and ts_code in ({1})) a left join t_tscode_company b on a.ts_code =b.ts_code order 
+    by value'''.format(last_cal_day, ts_code_set)
     feature_info_ema13_slope = pd.read_sql_query(sql_feature_ema13_slope, engine_finance_db)
 
     #   交易量排名
-    sql_amount_rank = "select company, amount_rank from (select ts_code, RANK() OVER (ORDER BY amount DESC) as amount_rank  from t_daily_info where trade_date ='{0}' limit 800) a left join t_tscode_company b on a.ts_code =b.ts_code"\
-        .format(last_cal_day)
+    sql_amount_rank = "select company, amount_rank from (select ts_code, RANK() OVER (ORDER BY amount DESC) as amount_rank  from t_daily_info where trade_date ='{0}' and ts_code in ({1})) a left join t_tscode_company b on a.ts_code =b.ts_code"\
+        .format(last_cal_day, ts_code_set)
     frame_amount_rank = pd.read_sql_query(sql_amount_rank, engine_finance_db)
 
-    feature_info = pd.merge(feature_info_ema65_slope, feature_info_ema13_slope, on=['company', 'trade_date'])
-    feature_info_amount_rank = pd.merge(feature_info, frame_amount_rank, on='company')
+    feature_info = pd.merge(feature_info_ema13_slope, feature_info_ema65_slope, on=['company', 'trade_date'])
+    feature_info_amount_rank = pd.merge(feature_info, frame_amount_rank, on='company').sort_values()
 
     send("趋势斜率", feature_info_amount_rank.to_html(), "{}日趋势斜率，最多展示前一个交易日长期趋势斜率大于0的200家企业，长期趋势斜率为EMA65近20日斜率，短期趋势为EMA13近10日斜率;此外还展示当日交易量的排名信息。".format(current_time))
