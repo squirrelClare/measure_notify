f43ec62c80aa91219a8e7f80f4c94d3e00a69b5b
zhangcc <zcc136314853@163.com>
2022-10-29 21:53:55 +0800

    ema简报新增mk检验

--- measure_SLOPE_EMA.py
+++ measure_SLOPE_EMA.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     from (select * from t_feature_numberic where field in ('SLOPE_EMA_13', 'CONFIDENCE_EMA_65', 'CONFIDENCE_EMA_13', 'TREND_EMA_65', 
     'TREND_EMA_13', 'CHANGE_TREND_EMA_65', 'CHANGE_TREND_EMA_13') and trade_date ='{0}' and ts_code in ({1})) a left join t_tscode_company b on a.ts_code =b.ts_code order 
     by value'''.format(last_cal_day, ts_code_set)
-    feature_info_multi = pd.read_sql_query(sql_feature_multi, engine_finance_db).pivot(index='company', columns='field', values='value').stack().reset_index()
+    feature_info_multi = pd.read_sql_query(sql_feature_multi, engine_finance_db).pivot(index='company', columns='field', values='value')#.stack().reset_index()
     print(feature_info_multi)
     #   交易量排名
     sql_amount_rank = "select company, amount_rank from (select ts_code, RANK() OVER (ORDER BY amount DESC) as amount_rank  from t_daily_info where trade_date ='{0}' and ts_code in ({1})) a left join t_tscode_company b on a.ts_code =b.ts_code" \
