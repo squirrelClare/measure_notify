d7aee5c038df85f1f7fa460549fb423027a80651
zhangcc <zcc136314853@163.com>
2022-10-30 09:54:59 +0800

    ema简报新增ema13二阶差分

--- measure_SLOPE_EMA.py
+++ measure_SLOPE_EMA.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     #   抽取特征数据
     sql_feature_multi = '''select company, field, value 
     from (select * from t_feature_numberic where field in ('SLOPE_EMA_13', 'CONFIDENCE_EMA_65', 'CONFIDENCE_EMA_13', 'TREND_EMA_65', 
-    'TREND_EMA_13', 'CHANGE_TREND_EMA_65', 'CHANGE_TREND_EMA_13') and trade_date ='{0}' and ts_code in ({1})) a left join t_tscode_company b on a.ts_code =b.ts_code order 
+    'TREND_EMA_13', 'CHANGE_TREND_EMA_65', 'CHANGE_TREND_EMA_13', 'DIFF_2_EMA_13') and trade_date ='{0}' and ts_code in ({1})) a left join t_tscode_company b on a.ts_code =b.ts_code order 
     by value'''.format(last_cal_day, ts_code_set)
     feature_info_multi = pd.read_sql_query(sql_feature_multi, engine_finance_db)
     feature_concat = pd.concat([feature_info_ema65_slope, feature_info_multi]).reset_index(drop=True).pivot(index='company', columns='field', values='value')
@@ -56,10 +56,10 @@ if __name__ == '__main__':
                                              'SLOPE_EMA_65': 'EMA65斜率', 'ts_code': '股票代码', 'amount_rank': '交易量排名',
                                              'CHANGE_TREND_EMA_13': 'EMA13_MK检验突变点个数', 'CHANGE_TREND_EMA_65': 'EMA65_MK检验突变点个数',
                                              'CONFIDENCE_EMA_13': 'EMA13_MK检验置信度', 'CONFIDENCE_EMA_65': 'EMA65_MK检验置信度',
-                                             'TREND_EMA_13': 'EMA13_MK检验趋势', 'TREND_EMA_65': 'EMA65_MK检验趋势'}, inplace=True)
+                                             'TREND_EMA_13': 'EMA13_MK检验趋势', 'TREND_EMA_65': 'EMA65_MK检验趋势', 'DIFF_2_EMA_13': 'EMA13二阶差分'}, inplace=True)
     # print(feature_info_amount_rank)
 
     send("趋势斜率", feature_info_amount_rank[['公司名', '股票代码', '交易日期', '交易量排名', 'EMA65斜率', 'EMA65_MK检验置信度',
                                            'EMA65_MK检验趋势', 'EMA65_MK检验突变点个数', 'EMA13斜率', 'EMA13_MK检验置信度',
-                                           'EMA13_MK检验趋势', 'EMA13_MK检验突变点个数']].to_html(),
+                                           'EMA13_MK检验趋势', 'EMA13_MK检验突变点个数', 'EMA13二阶差分']].to_html(),
          "{}日趋势斜率，最多展示前一个交易日长期趋势斜率大于0的200家企业，长期趋势斜率为EMA65近20日斜率，短期趋势为EMA13近10日斜率;此外还展示当日交易量的排名信息。".format(current_time))
